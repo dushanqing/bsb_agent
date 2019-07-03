db3d9089857b23b73cd13a7ab8cd6151e56732a7
yangjx-01 <31056351+yangjx-01@users.noreply.github.com>
2019-07-03 11:59:23 +0800

    增加经纬度定位-yangjx
    增加经纬度定位-yangjx

--- map/position/position.js
+++ map/position/position.js
@@ -22,11 +22,57 @@ Page({
         console.log("res.name:"+res.name);
         //选择地点之后返回到原来页面
         wx.navigateTo({
-          url: "/pages/mcht/mchtAdd/mchtBaseInfo/mchtBaseInfo?mchtContAddr=" + res.name
+          url: "/pages/mcht/mchtAdd/mchtBaseInfo/mchtBaseInfo?mchtContAddr=" + res.name + "&lng=" + res.longitude + "&lat=" + res.latitude,
         });
       },
       fail: function (err) {
-        console.log(err)
+        wx.getSetting({
+          success: function (res) {
+            var statu = res.authSetting;
+            if (!statu['scope.userLocation']) {
+              wx.showModal({
+                title: '是否授权当前位置',
+                content: '需要获取您的地理位置，请确认授权，否则地图功能将无法使用',
+                success: function (tip) {
+                  if (tip.confirm) {
+                    wx.openSetting({
+                      success: function (data) {
+                        if (data.authSetting["scope.userLocation"] === true) {
+                          wx.showToast({
+                            title: '授权成功',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                          //授权成功之后，再调用chooseLocation选择地方
+                          wx.chooseLocation({
+                            success: function (res) {
+                              obj.setData({
+                                addr: res.address
+                              })
+                            },
+                          })
+                        } else {
+                          wx.showToast({
+                            title: '授权失败',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                        }
+                      }
+                    })
+                  }
+                }
+              })
+            }
+          },
+          fail: function (res) {
+            wx.showToast({
+              title: '调用授权窗口失败',
+              icon: 'success',
+              duration: 1000
+            })
+          }
+        })
       }
     });
   }
